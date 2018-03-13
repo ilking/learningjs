9a35d30d34e1a368bec9f85796091cdcf4063b54
ilking <king.ian.ilk@gmail.com>
2018-03-13 14:04:39 -0400

    array features

--- learning.js
+++ learning.js
@@ -1,2 +1,9 @@
 let vals = [1,2,3];
+vals.push(44, 33, -11)
+console.log(vals);
+let res = vals.pop();
+console.log(vals);
+let res2 = vals.shift();
+console.log(vals);
+vals.splice(0,3, 3, 33, 44, 55);
 console.log(vals);
