1e030575bbd6f65f5f4091b69e760393023acfa1
ilking <king.ian.ilk@gmail.com>
2018-03-13 14:33:44 -0400

    added arrays

--- blackjack.js
+++ blackjack.js
@@ -1,7 +1,12 @@
-let card1 = "Ace of Spades",
-	card2 = "Ten of Hearts";
+let deck = [
+	"Ace of Spades",
+	"Two of Spades",
+	"Three of Spades"
+];
+
+let playerCards = [deck[0], deck[2]];
 
 console.log("Welcome to blackjack!");
 console.log("You are dealt...");
-console.log("\t" + card1);
-console.log("\t" + card2);
+console.log("\t" + playerCards[0]);
+console.log("\t" + playerCards[1]);
